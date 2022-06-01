d8db0b99bc14ed3126ff7346c146b8233194e703
marvin espira <marvinespira@adalabsafrica.com>
2022-06-01 18:55:12 +0300

    feat: ts update

--- two-sum.ts
+++ two-sum.ts
@@ -1,3 +1,13 @@
+/* EASY
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+EXAMPLE 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
 
 // let nums = [2,7,11,15]
 let nums = [15,11,2,8,7]
