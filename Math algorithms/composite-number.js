211faffd3b8cbde3722aef5f280a0e0853accd1d
marvin espira <espiramarvin@gmail.com>
2022-08-10 16:44:44 +0300

    composite number sol

---
+++ Math algorithms/composite-number.js
@@ -0,0 +1,4 @@
+/*
+A composite number - a number that is divisible by a number other than 1 and the number itself 
+eg. , 4 is a number that is divisible by 1, 2, and 4, therefore, it is a composite number.
+*/
