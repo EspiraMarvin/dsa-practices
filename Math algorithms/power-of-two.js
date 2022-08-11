b464b290f912da65c8cfa91eee4662cec357c620
marvin espira <espiramarvin@gmail.com>
2022-08-11 21:41:19 +0300

    power of two

---
+++ Math algorithms/power-of-two.js
@@ -0,0 +1,11 @@
+/*
+Given a positive integer `n`, determine if the number is a power of 2 or not
+
+-An integer is a power of two if there exists an integer `x` such that `n` ===2^x
+
+eg.
+ isPoweOfTwo(1) = true(2^0)
+ isPoweOfTwo(2) = true(2^1)
+ isPoweOfTwo(5) = false -
+
+*/
