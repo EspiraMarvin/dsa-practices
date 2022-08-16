582e5d76ee1abc53dc0c8124f7aa672f65b8c200
marvin espira <espiramarvin@gmail.com>
2022-08-16 10:41:19 +0300

    factorial of a number

--- Math algorithms/factorial.js
+++ Math algorithms/factorial.js
@@ -3,19 +3,19 @@ Given an integer `n`, find the factorial of that integer
 
 -In mathematics, the factorial of a non-negative integer `n`, denoted n!, is the product of all positive integer less than or equal to `n`
 
-Factorial of zero is 1
+Factorial of 0 or 0! is 1
 Factorial of 1 is 1
 
 eg.
-factorial(4) = 4*3*2*1 = 24
-factorial(5) = 5*4*3*2*1 = 120
+factorial(4) or 4! = 4*3*2*1 = 24
+factorial(5) or 5! = 5*4*3*2*1 = 120
 
 */
 /* 
 Solution - 
     - We loop over the fn argument/parameter `n` 
     - we create a variable to hold the product of all the numbers multiplied together. // let product = 1
-    - loop over n and mulitply each number with the result variable // result *= 1 same as result = result * 1
+    - loop over n and multiply each number with the result variable // result *= 1 same as result = result * 1
 
 */
 
