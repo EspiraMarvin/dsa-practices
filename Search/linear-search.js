6893ba23890a44f56ff290423a6c1d810829a6b9
marvin espira <espiramarvin@gmail.com>
2022-10-07 14:04:39 +0300

    linear search

--- Search/linear-search.js
+++ Search/linear-search.js
@@ -1,13 +1,16 @@
-/*
-Given an array of `n` elements and a target element `t`, find the index of `t` in the array. 
-Return -1 if the target element is not found.
-*/
-/*
-examples
+/**
+ * Linear search
+ * 
+ * Given an array of `n` elements and a target element `t`, find the index of `t` in the array. 
+ * Return -1 if the target element is not found.
+ * 
+ * examples
     arr = [-5,2, 10, 4, 6], t=10 -> should return 2
     arr = [-5,2, 10, 4, 6], t=6 -> should return 4
     arr = [-5,2, 10, 4, 6], t=10 -> should return -1
 */
+
+
 
 function linearSearch(arr, target) {
     for (let i=0; i < arr.length; i++) {
@@ -29,6 +32,7 @@ function linearSearchh(arr, target) {
 console.log('linearSearch 2', linearSearch([-5,2, 10, 4, 6], 10)) // 2
 console.log('linearSearch 6', linearSearch([-5,2, 10, 4, 6], 6)) // 4
 console.log('linearSearch 20', linearSearch([-5,2, 10, 4, 6], 20)) // -1
+
 
 
 /*
