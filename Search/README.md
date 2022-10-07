720dbaaeb3937afb6a6b5c09763f6616b3fb09f8
marvin espira <espiramarvin@gmail.com>
2022-10-07 13:58:22 +0300

    linear search

---
+++ Search/README.md
@@ -0,0 +1,7 @@
+## Search Algorithms
+
+### 1. Linear Search
+
+### 2. Binary Search
+
+### 3 Recursive Binary Search
---
+++ Search/linear-search.js
@@ -0,0 +1,38 @@
+/*
+Given an array of `n` elements and a target element `t`, find the index of `t` in the array. 
+Return -1 if the target element is not found.
+*/
+/*
+examples
+    arr = [-5,2, 10, 4, 6], t=10 -> should return 2
+    arr = [-5,2, 10, 4, 6], t=6 -> should return 4
+    arr = [-5,2, 10, 4, 6], t=10 -> should return -1
+*/
+
+function linearSearch(arr, target) {
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] === target){
+            return i
+        } 
+    }
+    return -1
+}
+
+function linearSearchh(arr, target) {
+    if (arr.indexOf(target)){
+        return arr.indexOf(target)
+    } else {
+        return -1
+    }
+}
+
+console.log('linearSearch 2', linearSearch([-5,2, 10, 4, 6], 10)) // 2
+console.log('linearSearch 6', linearSearch([-5,2, 10, 4, 6], 6)) // 4
+console.log('linearSearch 20', linearSearch([-5,2, 10, 4, 6], 20)) // -1
+
+
+/*
+BIG-O - O(n) linear time complexity
+
+
+*/
