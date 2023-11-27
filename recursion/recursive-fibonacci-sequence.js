837cc04bb9422a02c9c27409ad8c1e6f5a114624
EspiraMarvin <espiraMarvin@gmail.com>
2023-11-27 22:13:00 +0300

    add memoized fibonacci recursive soln

--- recursion/recursive-fibonacci-sequence.js
+++ recursion/recursive-fibonacci-sequence.js
@@ -16,21 +16,40 @@ F0 = 0 and F1 = 1
 */
 
 function recursiveFibonacci(n) {
-    if (n < 2) {             // this will return 0 if n=0 and will return 1 if n=1
-         return n    
-    }
-    return recursiveFibonacci(n -1) + recursiveFibonacci(n-2)
-
+  if (n < 2) {
+    // this will return 0 if n=0 and will return 1 if n=1
+    return n
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
 }
 
 console.log(recursiveFibonacci(0)) // 0
 console.log(recursiveFibonacci(1)) // 1
 console.log(recursiveFibonacci(6)) // 8
 console.log(recursiveFibonacci(7)) // 13
+// console.log(recursiveFibonacci(50)) // will take forever unless optimized by memoization fn below
 
 /*
 Recursive BIG-O -> O(2^n)
 Recursion is not a solution to fibonacci sequence problem
 */
 
+//recursive MEMOIZATION
+/**
+ *
+ * @param {*} n fibonacci number
+ * @param {*} memo JS object, keys will be arg to fn, value will be the return value
+ * @returns fib of n
+ */
+const fibMemoized = (n, memo = {}) => {
+  if (n in memo) return memo[n]
+  if (n <= 2) return 1
+  memo[n] = fibMemoized(n - 1, memo) + fibMemoized(n - 2, memo)
+  return memo[n]
+}
 
+console.log(fibMemoized(0)) // 0
+console.log(fibMemoized(1)) // 1
+console.log(fibMemoized(6)) // 8
+console.log(fibMemoized(7)) // 13
+console.log(fibMemoized(50)) //
